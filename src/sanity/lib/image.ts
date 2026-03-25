815f183416848a34a5f29eed13baa3c2fac9d366
Madhu Dadi <madhu.kumar245@gmail.com>
2026-03-25 19:46:03 +0530

    Final Deployment Version

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,11 +1,13 @@
-import createImageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
-
 import { dataset, projectId } from "../env";
+import { createClient } from "@sanity/client";
+import { createImageUrlBuilder } from "@sanity/image-url";
 
-// https://www.sanity.io/docs/image-url
-const builder = createImageUrlBuilder({ projectId, dataset });
+const client = createClient({
+  projectId,
+  dataset,
+  useCdn: true,
+});
 
-export const urlFor = (source: SanityImageSource) => {
-  return builder.image(source);
-};
+const builder = createImageUrlBuilder(client);
+
+export const urlFor = (source: any) => builder.image(source);
